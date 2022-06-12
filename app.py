a94a3608dcd562dc22fb76033e71ac42c0f6a633
Jayyp1234 <Okekejohnpaul12@gmail.com>
2022-06-12 07:54:54 +0100

    d

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 #Importing the necessary tools
-from flask import Flask,request, jsonify, render_template
+from flask import Flask,request, jsonify, render_template,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
 import numpy as np
 import pickle
 
-import pickle
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 #Loading the saved models in the ipynb file
 app = Flask(__name__)
@@ -13,6 +17,33 @@ sc=pickle.load(open('sc.pkl','rb'))
 label_encoder=pickle.load(open('label_encoder.pkl','rb'))
 
 #Creating url routing for the flask web app
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['UPLOAD_FOLDER'] = 'static/'
+
+
+db = SQLAlchemy(app)
+
+
+####MODELSSS
+
+class Crimes(db.Model):
+    __tablename__ = 'crimes '
+    id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.Integer, unique=False, nullable=False)
+    month = db.Column(db.Integer, unique=False, nullable=False)
+    year = db.Column(db.Integer, unique=False, nullable=False)
+    area = db.Column(db.String(150), unique=False, nullable=False)
+    crime = db.Column(db.String(150), unique=False, nullable=False)
+    timestamp = db.Column(db.DateTime, server_default=db.func.current_timestamp())
+
+    def __repr__(self):
+        return f"User('{self.day}', '{self.month}','{self.year}')"
+
+
+
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -31,10 +62,27 @@ def predict():
     return render_template('result.html',prediction_text=f'{transformed_prediction[0]}',store_month=month,store_year=year,store_area=area)
         
     
-@app.route('/contact')
+@app.route('/report_crime' ,methods=['POST','GET'])
 def contact():
+    if request.method == "POST":
+        form = request.form
+        day = request.form['day']
+        month = request.form['month']
+        year = request.form['year']
+        crime = request.form['crime']
+        location = request.form['location']
+        data = Crimes(day=day, month=month, year = year, crime = crime, area = location)
+        db.session.add(data)
+        db.session.commit()
+
+        return redirect(url_for('reports'))
     return render_template("contact.html")
 
 
+@app.route('/reports' ,methods=['POST','GET'])
+def reports():
+    reports = Crimes.query.all()
+    return render_template("reports.html", reports = reports)
+
 if __name__ == '__main__':
-   app.run(debug=False)
+   app.run(debug=True)
